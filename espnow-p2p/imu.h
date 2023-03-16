6c7b9a93f08f8c88eb740a5202b0705a7dbb19c3
Issouffa Ambdouloihi <49631741+Ambdouloihi@users.noreply.github.com>
2023-03-16 18:39:02 +0400

    Update imu.h

--- espnow-p2p/imu.h
+++ espnow-p2p/imu.h
@@ -7,28 +7,8 @@ public:
     imu();
     ~imu();
 
-<<<<<<< Updated upstream
-        float pitch;
-        float roll ;
-        float yaw ;
-
-        void getData() {};
-
-        uint8_t sendPitch();
-        uint8_t sendRoll();
-
-
-
-
-
-    
-   
-    
-=======
     float pitch;
     float roll;
     float yaw;
 
-    void getData();
->>>>>>> Stashed changes
 };
